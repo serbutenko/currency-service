1a8f06c1da8e5cea0d2e98059f8317e5535df89a
Sergey Butenko <s.butenko2006@yandex.ru>
2025-09-07 23:36:00 +0300

    Redis is enabled

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 
 	"os"
+	"strconv"
 
 	"encoding/json"
 	"net/http"
@@ -18,6 +19,8 @@ import (
 
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"example.com/currency-service/internal/cache"
+
 	currencyv1 "example.com/currency-service/api/currency/v1"
 )
 
@@ -25,6 +28,7 @@ var apiKey = os.Getenv("API_KEY")
 
 type server struct {
 	currencyv1.UnimplementedCurrencyServiceServer
+	redisCache *cache.RedisCache
 }
 
 type ConvertResponse struct {
@@ -46,28 +50,48 @@ type ListResponse struct {
 	List    map[string]string `json:"currencies"`
 }
 
-func (s *server) GetRate(ctx context.Context, req *currencyv1.GetRateRequest) (*currencyv1.GetRateResponse, error) {
-	from := req.GetFrom()
-	to := req.GetTo()
+func (s *server) RateFromApi(ctx context.Context, from string, to string) (float64, error) {
 	url := fmt.Sprintf(
 		"https://api.exchangerate.host/convert?access_key=%s&from=%s&to=%s&amount=1",
 		apiKey, from, to,
 	)
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return 0, fmt.Errorf("failed to get rate from API: %w", err)
 	}
 	defer resp.Body.Close()
 
 	var data ConvertResponse
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		panic(err)
+		return 0, fmt.Errorf("failed to convert reponse from API: %w", err)
+	}
+	rate := data.Info.Quote
+	err = s.redisCache.Set(ctx, fmt.Sprintf("rate:%s:%s", from, to), strconv.FormatFloat(rate, 'f', 6, 64), time.Hour)
+	if err != nil {
+		return 0, fmt.Errorf("failed to set rate in Redis: %w", err)
+	}
+	return rate, nil
+}
+
+func (s *server) GetRate(ctx context.Context, req *currencyv1.GetRateRequest) (*currencyv1.GetRateResponse, error) {
+	from := req.GetFrom()
+	to := req.GetTo()
+	var rate float64
+
+	val, err := s.redisCache.Get(ctx, fmt.Sprintf("rate:%s:%s", from, to))
+	if err != nil {
+		rate, err = s.RateFromApi(ctx, from, to)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		rate, _ = strconv.ParseFloat(val, 64)
 	}
 
 	return &currencyv1.GetRateResponse{
 		From:      req.GetFrom(),
 		To:        req.GetTo(),
-		Rate:      data.Info.Quote,
+		Rate:      rate,
 		Timestamp: timestamppb.New(time.Now().UTC()),
 	}, nil
 }
@@ -77,48 +101,67 @@ func (s *server) Convert(ctx context.Context, req *currencyv1.ConvertRequest) (*
 	to := req.GetTo()
 	amount := req.GetAmount()
 
-	url := fmt.Sprintf(
-		"https://api.exchangerate.host/convert?access_key=%s&from=%s&to=%s&amount=%f",
-		apiKey, from, to, amount,
-	)
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	var rate float64
 
-	var data ConvertResponse
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		panic(err)
+	val, err := s.redisCache.Get(ctx, fmt.Sprintf("rate:%s:%s", from, to))
+
+	if err != nil {
+		rate, err = s.RateFromApi(ctx, from, to)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		rate, _ = strconv.ParseFloat(val, 64)
 	}
+
+	result := amount * rate
 
 	return &currencyv1.ConvertResponse{
 		From:      req.GetFrom(),
 		To:        req.GetTo(),
 		Amount:    req.GetAmount(),
-		Converted: data.Result,
-		Rate:      data.Info.Quote,
+		Converted: result,
+		Rate:      rate,
 	}, nil
 }
 
 func (s *server) ListCurrencies(ctx context.Context, req *currencyv1.ListCurrenciesRequest) (*currencyv1.ListCurrenciesResponse, error) {
-	url := fmt.Sprintf(
-		"https://api.exchangerate.host/list?access_key=%s",
-		apiKey,
-	)
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+	var currencies map[string]string
+	val, err := s.redisCache.Get(ctx, "currencies:list")
 
-	var data ListResponse
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		panic(err)
+	if err != nil {
+		url := fmt.Sprintf(
+			"https://api.exchangerate.host/list?access_key=%s",
+			apiKey,
+		)
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get rate from API: %w", err)
+		}
+		defer resp.Body.Close()
+
+		var data ListResponse
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			return nil, fmt.Errorf("failed to convert reponse from API: %w", err)
+		}
+
+		currencies = data.List
+
+		jsonData, err := json.Marshal(currencies)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal currencies: %w", err)
+		}
+
+		err = s.redisCache.Set(ctx, "currencies:list", string(jsonData), time.Hour)
+		if err != nil {
+			return nil, fmt.Errorf("failed to set rate in Redis: %w", err)
+		}
+	} else {
+		json.Unmarshal([]byte(val), &currencies)
 	}
 
 	return &currencyv1.ListCurrenciesResponse{
-		Currencies: data.List,
+		Currencies: currencies,
 	}, nil
 }
 
@@ -133,8 +176,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	rdb := cache.NewRedisCache("localhost:6379", "", 0)
+	s := &server{redisCache: rdb}
+
 	grpcServer := grpc.NewServer()
-	currencyv1.RegisterCurrencyServiceServer(grpcServer, &server{})
+	currencyv1.RegisterCurrencyServiceServer(grpcServer, s)
 
 	reflection.Register(grpcServer)
 
